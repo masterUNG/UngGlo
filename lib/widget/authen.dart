a057679b0f8379d8e20d45de091890c4797ca691
masterUNG <phrombutr@gmail.com>
2020-03-02 13:29:25 +0700

    After Lunch

---
+++ lib/widget/authen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  // Field
+
+  // Method
+
+  Widget showLogo() {
+    return Container(
+      height: 120.0,
+      width: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text('Ung Glo');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            showLogo(),
+            showAppName(),
+          ],
+        ),
+      ),
+    );
+  }
+}
