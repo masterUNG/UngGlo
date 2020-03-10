98710394aa7ce556f63491c2c926ae4378b0b580
masterUNG <phrombutr@gmail.com>
2020-03-10 15:24:56 +0700

    Project Success

--- lib/container/show_map.dart
+++ lib/container/show_map.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class ShowMap extends StatefulWidget {
   @override
@@ -7,8 +7,42 @@ class ShowMap extends StatefulWidget {
 }
 
 class _ShowMapState extends State<ShowMap> {
+  // Field
+  static LatLng gloCenter = LatLng(13.880496, 100.504845);
+  CameraPosition cameraPosition = CameraPosition(
+    target: gloCenter,
+    zoom: 16.0,
+  );
+
+  // Method
+  Marker gloMarker() {
+    return Marker(
+      infoWindow: InfoWindow(
+        title: 'สำนักงาน สลาก',
+        snippet: '123/456 ถนนติวานน กทม',
+      ),
+      markerId: MarkerId('glo'),
+      position: gloCenter,
+    );
+  }
+
+  Set<Marker> setMarker() {
+    return <Marker>[
+      gloMarker(),
+    ].toSet();
+  }
+
+  Widget showGoogleMap() {
+    return GoogleMap(
+      markers: setMarker(),
+      initialCameraPosition: cameraPosition,
+      mapType: MapType.normal,
+      onMapCreated: (GoogleMapController controller) {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Show Map');
+    return showGoogleMap();
   }
 }
